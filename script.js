eb7aa7e08d99ebc27795ee4a73ba3df4364f55c1
YuliiaChernova <69728470+YuliiaChernova@users.noreply.github.com>
2020-09-29 17:17:57 +0300

    Add homework 18

--- script.js
+++ script.js
@@ -5,28 +5,45 @@ const albumTemplate = document.getElementById('albumTemplate').innerHTML;
 const photoTemplate = document.getElementById('photoTemplate').innerHTML;
 
 const albumsContainer = document.getElementById('albums-container');
+const photosContainer = document.getElementById('photos-container');
 
 let albumsList = [];
+let photosList = [];
 
 function getAlbums() {
     fetch('https://jsonplaceholder.typicode.com/albums')
         .then((response) => response.json())
-        .then((data) => (albumsList = data));
+        .then((data) => (albumsList = data))
+        .then(renderAllAlbums);
+}
+
+function getPhotos(id) {
+    fetch('https://jsonplaceholder.typicode.com/photos?albumId='+id)
+        .then((response) => response.json())
+        .then((data) => (photosList = data))
+        .then(renderAllPhotos);
 }
 
 function renderOneAlbum(album) {
-    const htmlAlbum = albumTemplate.replace('{{album-title}}', album.title);
-
-    console.log(htmlAlbum);
+    const htmlAlbum = albumTemplate.replace('{{album-title}}', album.title)
+                                   .replace('{{album-id}}', album.id);
     albumsContainer.insertAdjacentHTML('beforeend', htmlAlbum);
 }
 
-function renderAllAlbums(albums) {
-    for (let i = 0; i < albums.length; i++) {
-        renderOneAlbum(albums[i]);
-    }
+function renderAllAlbums() {
+    albumsList.forEach(album => renderOneAlbum(album));
+}
+
+function renderOnePhoto(photo) {
+    const htmlPhoto = photoTemplate.replace('{{photo-url}}', photo.url)
+                                   .replace('{{photo-title}}', photo.title);
+
+    photosContainer.insertAdjacentHTML('beforeend', htmlPhoto);
+}
+
+function renderAllPhotos() {
+    photosList.forEach(photo => renderOnePhoto(photo));
 }
 
 getAlbums();
-
-renderAllAlbums(albumsList);
+getPhotos(albumsList[0].id);

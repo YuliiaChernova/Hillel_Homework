0d6c1f5e59a8cad934211f4cc57def1fee8f828c
YuliiaChernova <69728470+YuliiaChernova@users.noreply.github.com>
2020-09-29 17:55:42 +0300

    Add homework 18

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ALBUM_CLASS = 'album';
-const PHOTO_CASS = 'photo';
+//const PHOTO_CASS = 'photo';
 
 const albumTemplate = document.getElementById('albumTemplate').innerHTML;
 const photoTemplate = document.getElementById('photoTemplate').innerHTML;
@@ -10,11 +10,31 @@ const photosContainer = document.getElementById('photos-container');
 let albumsList = [];
 let photosList = [];
 
+albumsContainer.addEventListener('click', onAlbumsContainerClick);
+
+function onAlbumsContainerClick(event) {
+    if(event.target.classList.contains(ALBUM_CLASS)) {
+        showAlbumPhotos(event.target);
+    }
+}
+
+function showAlbumPhotos(el) {
+    const albumId = +el.dataset.albumId;
+    console.log(albumId);
+    getPhotos(albumId);
+}
+
+
 function getAlbums() {
     fetch('https://jsonplaceholder.typicode.com/albums')
         .then((response) => response.json())
         .then((data) => (albumsList = data))
+        .then(showPhotosFirstTime)
         .then(renderAllAlbums);
+}
+
+function showPhotosFirstTime() {
+    getPhotos(albumsList[0].id);
 }
 
 function getPhotos(id) {
@@ -46,4 +66,3 @@ function renderAllPhotos() {
 }
 
 getAlbums();
-getPhotos(albumsList[0].id);

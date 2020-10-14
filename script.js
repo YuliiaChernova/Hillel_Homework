b8ee8cdc9c9057bbcefbab1bafada23b2cfdf9c2
YuliiaChernova <69728470+YuliiaChernova@users.noreply.github.com>
2020-10-14 19:55:19 +0300

    Start homework

---
+++ script.js
@@ -0,0 +1,9 @@
+$(() => {
+    const CONTACTS_URL = 'http://5dd3d5ba8b5e080014dc4bfa.mockapi.io/contacts/';
+
+    const $contactTemplate = $('#template').html();
+
+    const date = $('#date');
+
+    //date.
+});

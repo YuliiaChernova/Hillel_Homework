2aeca68e790775aff8ae4215645efdfacad6a452
YuliiaChernova <69728470+YuliiaChernova@users.noreply.github.com>
2020-10-19 19:30:25 +0300

    Finished Homework
    Плагин https://fotorama.io/

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 $(() => { 
-    const $photoTemplate = $('#photoTemplate');
+    const $fotoramaDiv = $('#fotorama').fotorama();
+    const fotorama = $fotoramaDiv.data('fotorama');
 
-    const $galleryContainer = $('.gallery-container');
+    let albumsList = [];
+    let photosList = [];
+    let arrToAdd = [];
 
+    init();
+    
+    function init() {
+        getAlbums();
+    }
+
+    function getAlbums() {
+        fetch('https://jsonplaceholder.typicode.com/albums')
+            .then((response) => response.json())
+            .then((data) => (albumsList = data))
+            .then(showPhotos);
+    }
+
+    function showPhotos() {
+        getPhotos(albumsList[0].id);
+    }
+
+    function getPhotos(id) {
+        fetch('https://jsonplaceholder.typicode.com/photos?albumId='+id)
+            .then((response) => response.json())
+            .then((data) => (photosList = data))
+            .then(() => createObjectsArrToAdd(photosList))
+            .then(() => fotorama.load(arrToAdd));
+    }
+
+    function createImgObjTemplate(obj) {
+        const objToAdd = {
+            img: obj.url,
+            thumb: obj.thumbnailUrl,
+        }
+
+        return objToAdd;
+    }
+
+    function createObjectsArrToAdd(arr) {
+
+        for (let i = 0; i < arr.length; i++) {
+            arrToAdd.push(createImgObjTemplate(arr[i]));
+        }
+    }
 });
